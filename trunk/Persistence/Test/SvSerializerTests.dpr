4ced4abffdff21d070726db6b5af6dd423ba4fbf
lnaginionis@gmail.com <lnaginionis@gmail.com@f9ce7c4e-b02a-73fc-42a8-20e0c804bbeb>
2012-12-06 14:51:14 +0000

    + Added Persistence support

---
+++ trunk/Persistence/Test/SvSerializerTests.dpr
@@ -0,0 +1,40 @@
+program SvSerializerTests;
+{
+
+  Delphi DUnit Test Project
+  -------------------------
+  This project contains the DUnit test framework and the GUI/Console test runners.
+  Add "CONSOLE_TESTRUNNER" to the conditional defines entry in the project options
+  to use the console test runner.  Otherwise the GUI test runner will be used by
+  default.
+
+}
+
+{$IFDEF CONSOLE_TESTRUNNER}
+{$APPTYPE CONSOLE}
+{$ENDIF}
+
+uses
+  Forms,
+  TestFramework,
+  GUITestRunner,
+  TextTestRunner,
+  TestSvSerializerJsonFactory in 'TestSvSerializerJsonFactory.pas',
+  SvSerializerJsonFactory in '..\SvSerializerJsonFactory.pas',
+  SvSerializer in '..\SvSerializer.pas',
+  SQLite3 in 'SQLite3.pas',
+  SQLiteTable3 in 'SQLiteTable3.pas',
+  SvSerializer.Extensions.SQLite in '..\SvSerializer.Extensions.SQLite.pas';
+
+{$R *.RES}
+
+begin
+  Application.Initialize;
+  ReportMemoryLeaksOnShutdown := True;
+  if IsConsole then
+    with TextTestRunner.RunRegisteredTests do
+      Free
+  else
+    GUITestRunner.RunRegisteredTests;
+end.
+
