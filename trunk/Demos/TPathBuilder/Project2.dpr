e39e99a2b48305555dddd03495ef947dfda56ae4
lnaginionis@gmail.com <lnaginionis@gmail.com@f9ce7c4e-b02a-73fc-42a8-20e0c804bbeb>
2012-12-12 13:59:44 +0000

    + More Demos + Restored SvTesting.DUnit compatibility with Delphi XE + Some improvements in TPathBuilder

---
+++ trunk/Demos/TPathBuilder/Project2.dpr
@@ -0,0 +1,17 @@
+// JCL_DEBUG_EXPERT_GENERATEJDBG OFF
+// JCL_DEBUG_EXPERT_INSERTJDBG OFF
+// JCL_DEBUG_EXPERT_DELETEMAPFILE OFF
+program Project2;
+
+uses
+  Forms,
+  Unit3 in 'Unit3.pas' {Form3};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm3, Form3);
+  Application.Run;
+end.
