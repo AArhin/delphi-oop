6429936be19d21b2f04ef1ed7df96797b2182577
lnaginionis@gmail.com <lnaginionis@gmail.com@f9ce7c4e-b02a-73fc-42a8-20e0c804bbeb>
2013-09-04 13:23:32 +0000

    Added Web unit tests.

---
+++ trunk/Web/Tests/WebTests.dpr
@@ -0,0 +1,27 @@
+program WebTests;
+
+
+{$IFDEF CONSOLE_TESTRUNNER}
+{$APPTYPE CONSOLE}
+{$ENDIF}
+
+uses
+  Forms,
+  TestFramework,
+  GUITestRunner,
+  TextTestRunner,
+  RESTClientTest in 'RESTClientTest.pas',
+  REST.Client in '..\REST.Client.pas';
+
+{$R *.RES}
+
+begin
+  Application.Initialize;
+  if IsConsole then
+    with TextTestRunner.RunRegisteredTests do
+      Free
+  else
+    GUITestRunner.RunRegisteredTests;
+
+end.
+
